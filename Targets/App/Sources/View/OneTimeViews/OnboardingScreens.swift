1f343b9073fe2ef8d46f83522dc65ae57ab2526f
Aize Igbinakenzua <aizeakenzua@gmail.com>
2025-04-01 12:14:34 +0100

    Refactor ReminderScreen layout for improved user experience
    - Updated the `ReminderScreen` to utilize `GeometryReader`, enhancing the layout adaptability and ensuring the DatePicker is properly sized within the available space.
    - Added vertical spacers to improve visual balance and user interaction with the DatePicker.
    - These changes aim to create a more engaging and user-friendly onboarding experience, aligning with the application's design objectives.

--- Targets/App/Sources/View/OneTimeViews/OnboardingScreens.swift
+++ Targets/App/Sources/View/OneTimeViews/OnboardingScreens.swift
@@ -414,15 +414,23 @@ struct ReminderScreen: View {
                 viewModel.moveToPreviousScreen()
             }
         ) {
-            VStack(spacing: 24) {
-                DatePicker("Select Time", selection: $viewModel.selections.reminderTime, displayedComponents: .hourAndMinute)
-                    .datePickerStyle(.wheel)
-                    .labelsHidden()
-                    .colorScheme(.dark)
-                    .accentColor(.brandPrimary)
-                    .background(Color.baseBlack)
+            GeometryReader { geometry in
+                VStack {
+                    Spacer()
+                    
+                    DatePicker("Select Time", selection: $viewModel.selections.reminderTime, displayedComponents: .hourAndMinute)
+                        .datePickerStyle(.wheel)
+                        .labelsHidden()
+                        .colorScheme(.dark)
+                        .accentColor(.brandPrimary)
+                        .background(Color.baseBlack)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: geometry.size.height * 35)
+                    
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
-            .padding(.vertical, 20)
         }
     }
 }
